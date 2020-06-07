66841026c39a09f811c953e32479cbf870e251de
Joel Nuñez <joelnun3z@gmail.com>
2020-06-07 00:50:40 -0700

    fixing the bug on the nav

--- public/index.js
+++ public/index.js
@@ -1,12 +1,15 @@
 const aside = document.querySelector('aside')
 const burger = document.querySelector('.active')
 const close = document.querySelector('.close')
+const body = document.querySelector('body')
 
 burger.addEventListener('click', () => {
   if (burger.classList.contains('active')) {
     aside.classList.remove('translateY')
+    body.style = 'overflow:hidden;'
   } else {
     aside.classList.add('translateY')
+    body.style = 'overflow:none;'
   }
 })
 
@@ -14,8 +17,10 @@ close.addEventListener('click', (e) => {
   console.log(e.target)
   if (close.classList.contains('active')) {
     aside.classList.remove('translateY')
+    body.style = 'overflow:hidden;'
   } else {
     aside.classList.add('translateY')
+    body.style = 'overflow:none;'
   }
 })
 
